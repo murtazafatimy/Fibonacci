7d82ab6ef2d844a396d432dbec5910eba91b8cad
Murtaza Fatimy <murt@Murtazas-MBP.lan1>
2022-11-30 17:17:33 -0800

    Initial commit

---
+++ src/main/java/edu/sdccd/cisc191/Fibonacci.java
@@ -0,0 +1,21 @@
+package edu.sdccd.cisc191;
+
+import java.util.Scanner;
+public class Fibonacci
+{
+    public static int fibonacci(int n) {
+        if (n < 0)
+            return -1;
+        if(n==0 || n==1)
+            return n;
+
+        return fibonacci(n-1) + fibonacci(n-2);
+    }
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        int startNum;
+
+        startNum = scnr.nextInt();
+        System.out.println("fibonacci(" + startNum + ") is " + fibonacci(startNum));
+    }
+}
